0cf7106a30f9c5b9c0c9ded7dcb2323374435a33
Eissa Faheem <eissafaheem@gmail.com>
2023-10-07 18:58:27 +0530

    Feat: Implemented landing page

--- react-frontend/src/app/screens/landing-page/LandingPage.component.tsx
+++ react-frontend/src/app/screens/landing-page/LandingPage.component.tsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import brandLogo from '../../../assets/brand-logo.svg'
+import LandingStyles from './LandingPage.module.css'
+import illustration from '../../../assets/landing-page-illustration.svg'
+import ButtonComponent from '../../components/button/Button.component'
+import { useLandingPageHook } from './LandingPage.hook'
+import InputComponent from '../../components/input/Input.component'
 
 function LandingPageComponent() {
+
+  const {setRoomId} = useLandingPageHook();
+
   return (
-    <div>
-      Landing
+    <div className={LandingStyles['landing-main-container']}>
+      <header>
+        <img src={brandLogo} alt="Brand Logo" />
+        <h1>
+          Vision
+        </h1>
+      </header>
+      <div className={LandingStyles["content"]}>
+        <div className={LandingStyles["brand-info"]}>
+          <div>
+            <h1>
+              Bring people <br /> together to make <br /> visions happen
+            </h1>
+            <p>Revolutionary video calling app for design and code reviews. <br /> With Vision, you ‘ll never to leave you house again.</p>
+          </div>
+          <div className={LandingStyles["functionalities"]}>
+            <ButtonComponent text='Create' type='primary' onClick={() => { }} />
+            <form className={LandingStyles['meeting-form']}>
+              <InputComponent placeholder='Enter meeting code' setValue={setRoomId} type='text'/>
+              <ButtonComponent text='Join' type='secondary' onClick={() => { }} />
+            </form>
+          </div>
+        </div>
+        <img src={illustration} className={LandingStyles['illustration']} alt="Video calling" />
+      </div>
     </div>
   )
 }
--- react-frontend/src/app/screens/landing-page/LandingPage.hook.ts
+++ react-frontend/src/app/screens/landing-page/LandingPage.hook.ts
@@ -1,5 +1,10 @@
+import { useState } from "react"
+
 export function useLandingPageHook(){
+
+    const [roomId, setRoomId]  = useState<string>("");
+
     return {
-        
+        setRoomId
     }
 }
