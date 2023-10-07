ac0b373a7944806d7e120e81b50dafc82d6b061a
Eissa Faheem <eissafaheem@gmail.com>
2023-10-07 20:51:19 +0530

    Feat: Room Component ui completed

--- react-frontend/src/app/screens/room/Room.component.tsx
+++ react-frontend/src/app/screens/room/Room.component.tsx
@@ -1,13 +1,57 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import RoomStyles from './Room.module.css'
+import micOn from './../../../assets/mic-on.svg'
+import micOff from './../../../assets/mic-off.svg'
+import videoOff from './../../../assets/video-off.svg'
+import videoOn from './../../../assets/video-on.svg'
+import callEnd from './../../../assets/call-end.svg'
+import { useRoomPageHook } from './Room.hook'
 
 function RoomComponent() {
 
   const { roomId } = useParams();
+  const {
+    handleMicClick,
+    handleVideoClick,
+    isMicOn,
+    isVideoOn,
+    isControlsVisible,
+    handleControlsVisible,
+    isOtherMicOn,
+    isOtherVideoOn,
+    handleEndCall
+  } = useRoomPageHook();
 
   return (
-    <div>
-      {roomId}hii
+    <div className={RoomStyles['room-main-container']} onMouseMove={handleControlsVisible}>
+      <div className={RoomStyles["all-streams"]}>
+        <div className={RoomStyles["my-stream"]}>
+
+        </div>
+        <div className={RoomStyles["other-stream"]}>
+          <div className={RoomStyles["other-controls"]}>
+            <img src={isOtherMicOn ? micOn : micOff} alt="Mic"
+              className={`${isMicOn ? RoomStyles['active'] : RoomStyles['inactive']}`}
+              onClick={handleMicClick}
+            />
+            <img src={isVideoOn ? videoOn : videoOff} alt="Video"
+              className={`${isOtherVideoOn ? RoomStyles['active'] : RoomStyles['inactive']}`}
+              onClick={handleVideoClick} />
+          </div>
+        </div>
+        <div className={`${RoomStyles["my-controls"]} ${!isControlsVisible && RoomStyles["hide-my-controls"]}`}>
+          <img src={isMicOn ? micOn : micOff} alt="Mic"
+            className={`${isMicOn ? RoomStyles['active'] : RoomStyles['inactive']}`}
+            onClick={handleMicClick}
+          />
+          <img src={callEnd} alt="Call End" className={RoomStyles['inactive']} 
+          onClick={handleEndCall}/>
+          <img src={isVideoOn ? videoOn : videoOff} alt="Video"
+            className={`${isVideoOn ? RoomStyles['active'] : RoomStyles['inactive']}`}
+            onClick={handleVideoClick} />
+        </div>
+      </div>
     </div>
   )
 }
--- react-frontend/src/app/screens/room/Room.hook.ts
+++ react-frontend/src/app/screens/room/Room.hook.ts
@@ -1,5 +1,51 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom";
+
 export function useRoomPageHook(){
+
+    const navigate = useNavigate();
+    const [isControlsVisible, setIsControlsVisible] = useState<boolean>(true);
+    const [isMicOn, setIsMicOn] = useState<boolean>(true);
+    const [isVideoOn, setIsVideoOn] = useState<boolean>(true);
+    const [isOtherMicOn, setIsOtherMicOn] = useState<boolean>(true);
+    const [isOtherVideoOn, setIsOtherVideoOn] = useState<boolean>(true);
+    let controlsTimer: NodeJS.Timeout;
+
+    useEffect(()=>{
+        console.log(isControlsVisible)
+    },[isControlsVisible])
+
+    function handleControlsVisible(){
+        if(controlsTimer){
+            clearTimeout(controlsTimer);
+        }
+        setIsControlsVisible(true);
+        controlsTimer = setTimeout(()=>{
+            setIsControlsVisible(false);
+        },3000)
+    }
+
+    function handleEndCall(){
+        navigate('/');
+    }
+
+    function handleMicClick(){
+        setIsMicOn(!isMicOn)
+    }
+
+    function handleVideoClick(){
+        setIsVideoOn(!isVideoOn)
+    }
+
     return {
-        
+        isMicOn,
+        isVideoOn,
+        handleMicClick,
+        handleVideoClick,
+        isControlsVisible,
+        handleControlsVisible,
+        isOtherMicOn,
+        isOtherVideoOn,
+        handleEndCall
     }
 }
